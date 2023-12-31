c920ac57be589620b9954a4ee5910477ca4f40f0
Angelos Zaimis <angeloszaimis@Angeloss-MacBook-Air.local>
2023-12-31 20:19:54 +0100

    feat: changes to fix android issues

--- App.tsx
+++ App.tsx
@@ -1,26 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import useSplashScreen from "./splashScreen/SplashScreen";
 import RootNavigation from "./navigation";
 import { AuthProvider } from "./hooks/auth/AuthContext";
 import { LanguageProvider } from "./components/util/LangContext";
-import * as SplashScreen from 'expo-splash-screen';
 import { LogBox } from "react-native";
 
-LogBox.ignoreAllLogs();
+LogBox.ignoreAllLogs();  // Consider reviewing this for development purposes
 
 export default function App() {
-
-  /**
-   * Use splash screen
-   */
-
   const { appIsReady, onLayoutRootView } = useSplashScreen();
+
+  // Call onLayoutRootView when appIsReady changes to true
+  useEffect(() => {
+    if (appIsReady) {
+      onLayoutRootView();
+    }
+  }, [appIsReady, onLayoutRootView]);
 
   if (!appIsReady) {
     return null;
-  } 
-  onLayoutRootView()
-  
+  }
+
   return (
     <AuthProvider>
       <LanguageProvider>
@@ -29,4 +29,3 @@ export default function App() {
     </AuthProvider>
   );
 }
-
--- splashScreen/SplashScreen.tsx
+++ splashScreen/SplashScreen.tsx
@@ -1,32 +1,28 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
-import Entypo from '@expo/vector-icons/Entypo';
 import * as SplashScreen from 'expo-splash-screen';
 import * as Font from 'expo-font';
 import { useFonts } from 'expo-font';
 
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync();  // Prevent the splash screen from auto hiding
 
 export default function useSplashScreen() {
-  const [appIsReady, setAppIsReady] = useState<boolean>(false);
+  const [appIsReady, setAppIsReady] = useState(false);
 
-  const [fontsLoaded, fontError] = useFonts({
+  const [fontsLoaded] = useFonts({
     'Inter-Black': require('../assets/fonts/Inter-Black.otf'),
   });
 
   useEffect(() => {
     async function prepare() {
       try {
-        // Pre-load fonts, make any API calls you need to do here
-        await Font.loadAsync(Entypo.font);
-        // Artificially delay for two seconds to simulate a slow loading
-        // experience. Please remove this if you copy and paste the code!
-        await new Promise(resolve => setTimeout(resolve, 1000));
+        await Font.loadAsync({
+          // Any other fonts you want to load would go here
+        });
+        // Wait for fonts to be loaded before setting app ready
       } catch (e) {
         console.warn(e);
       } finally {
-        // Tell the application to render
-        setAppIsReady(true);
+        setAppIsReady(true);  // Set app as ready
       }
     }
 
@@ -35,11 +31,6 @@ export default function useSplashScreen() {
 
   const onLayoutRootView = useCallback(async () => {
     if (appIsReady) {
-      // This tells the splash screen to hide immediately! If we call this after
-      // `setAppIsReady`, then we may see a blank screen while the app is
-      // loading its initial state and rendering its first pixels. So instead,
-      // we hide the splash screen once we know the root view has already
-      // performed layout.
       await SplashScreen.hideAsync();
     }
   }, [appIsReady]);
